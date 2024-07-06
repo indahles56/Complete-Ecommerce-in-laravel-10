11288545692fe9ee5a369431fded0c5b01f256eb
Indah Lestari <indahlestari05@mhs.pelitabangsa.ac.id>
2024-07-06 14:37:43 +0700

    home: ok

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -123,7 +123,7 @@
                                                 @elseif($product->condition=='Terbaik')
                                                     <span class="hot">Tren</span>
                                                 @else
-                                                    <span class="price-dec">{{$product->discount}}% </span>
+                                                    {{-- <span class="price-dec">{{$product->discount    }}% </span> --}}
                                                 @endif
 
 
@@ -144,8 +144,8 @@
                                                 @php
                                                     $after_discount=($product->price-($product->price*$product->discount)/100);
                                                 @endphp
-                                                <span>${{number_format($after_discount,2)}}</span>
-                                                <del style="padding-left:4%;">${{number_format($product->price,2)}}</del>
+                                                <span>Rp{{number_format($after_discount,2)}}</span>
+                                                {{-- <del style="padding-left:4%;">${{number_format($product->price,2)}}</del> --}}
                                             </div>
                                         </div>
                                     </div>
@@ -235,11 +235,11 @@
                             <div class="product-content">
                                 <h3><a href="{{route('product-detail',$product->slug)}}">{{$product->title}}</a></h3>
                                 <div class="product-price">
-                                    <span class="old">${{number_format($product->price,2)}}</span>
+                                    {{-- <span class="old">{{number_format($product->price,2)}}</span> --}}
                                     @php
                                     $after_discount=($product->price-($product->price*$product->discount)/100)
                                     @endphp
-                                    <span>${{number_format($after_discount,2)}}</span>
+                                    <span>Rp{{number_format($after_discount,2)}}</span>
                                 </div>
                             </div>
                         </div>
@@ -287,7 +287,10 @@
                                 <div class="col-lg-6 col-md-6 col-12 no-padding">
                                     <div class="content">
                                         <h4 class="title"><a href="#">{{$product->title}}</a></h4>
-                                        <p class="price with-discount">${{number_format($product->discount,2)}}</p>
+                                        @php
+                                            $after_discount=($product->price-($product->price*$product->discount)/100)
+                                        @endphp
+                                        <p class="price with-discount">Rp{{number_format($after_discount,2)}}</p>
                                     </div>
                                 </div>
                                 </div>
