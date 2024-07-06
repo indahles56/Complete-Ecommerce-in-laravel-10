e302104036e8192aa28a1f76e92a5258338a6dfe
Indah Lestari <indahlestari05@mhs.pelitabangsa.ac.id>
2024-07-06 15:32:19 +0700

    ganti dollar dengan rupiah

--- resources/views/frontend/pages/product_detail.blade.php
+++ resources/views/frontend/pages/product_detail.blade.php
@@ -84,7 +84,7 @@
                                                 @php 
                                                     $after_discount=($product_detail->price-(($product_detail->price*$product_detail->discount)/100));
                                                 @endphp
-												<p class="price"><span class="discount">${{number_format($after_discount,2)}}</span><s>${{number_format($product_detail->price,2)}}</s> </p>
+												<p class="price"><span class="discount">Rp{{number_format($after_discount,2)}}</span></p>
 												<p class="description">{!!($product_detail->summary)!!}</p>
 											</div>
 											<!--/ End Description -->
